9d2325eb9fcbfd9a4d2fcfaf27ffda83decd71f4
Edgar Ordóñez <edrodriguez2310@gmail.com>
2018-02-21 08:17:28 +0100

    first commit

---
+++ src/containers/app/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://graphql.kiwi.com/' }),
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <h1>React</h1>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
